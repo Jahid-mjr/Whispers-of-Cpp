0c2400a6f6d7acf7845f063f4019635497ad143c
Muhammad Jahidul Islam <mjr.jahidulislam@gmail.com>
2025-12-05 18:07:48 +0600

    Add files via upload

---
+++ Array Of Objects/sort-array_marks_soman_hole.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+
+    public:
+    string name;
+    int roll;
+    int marks;
+
+
+};
+
+
+bool cmp(Student l, Student r){
+
+    // if (l.marks > r.marks)
+    // {
+    //     return true;
+    // }
+
+    // else if (l.marks < r.marks)
+    // {
+    //     return false;
+    // }
+    
+    // else{
+
+    //     // if (l.roll < r.roll)
+    //     // {
+    //     //     return true;
+    //     // }
+    //     // else{
+    //     //     return false;
+    //     // }
+        
+    //     return l.roll < r.roll;
+        
+    // }
+
+
+
+
+    // full shortcut
+
+    // if (l.marks = r.marks)
+    // {
+    //     return l.roll < r.roll;
+    // }
+
+    // else{
+
+    //     return l.marks > r.marks;
+    // }
+
+
+
+
+    // tarnerny diya
+
+    return (l.marks = r.marks) ? l.roll < r.roll : l.marks > r.marks;
+
+
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false); // Speeds up I/O
+    //cin.tie(nullptr);
+
+
+    int n;
+    cin >> n;
+
+    Student a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+    
+
+    sort(a,a+n,cmp);
+
+    for (int i = 0; i < n; i++)
+    {
+
+       cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    }
+    
+
+    
+
+    
+    return 0;
+}
---
+++ Array Of Objects/sort_array_2.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+
+    public:
+    string name;
+    int roll;
+    int marks;
+
+
+};
+
+
+bool cmp(Student l, Student r){
+
+    // if (l.roll < r.roll)
+    // {
+    //     return true;
+    // }
+    // else{
+
+    //     return false;
+    // }
+    
+    return l.roll > r.roll;
+
+
+
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false); // Speeds up I/O
+    //cin.tie(nullptr);
+
+
+    int n;
+    cin >> n;
+
+    Student a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i].name >> a[i].roll >> a[i].marks;
+    }
+    
+
+    sort(a,a+n,cmp);
+
+    for (int i = 0; i < n; i++)
+    {
+
+       cout << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
+    }
+    
+
+    
+
+    
+    return 0;
+}
